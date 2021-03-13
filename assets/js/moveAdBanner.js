1b58f29290def236d3697a72dab7582785bd1ed7
Georgi <georgikolev@mail.bg>
2021-03-13 22:03:35 +0200

    added functionality for adbanner in home page

---
+++ assets/js/moveAdBanner.js
@@ -0,0 +1,10 @@
+let divAdBanner = getById("adBannerHomePage-box");
+let counter = 0;
+(function moveAdBanner () {
+    divAdBanner.style.marginLeft = counter + "px"
+    counter -= 1.5;
+    requestAnimationFrame(moveAdBanner);
+    if (counter < -5600){
+        counter = 940;
+    }
+})()
