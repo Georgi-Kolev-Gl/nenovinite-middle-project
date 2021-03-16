ae5fff86daf07f2cf473418e717b3e3fbe8dd0af
Kiril Zashev <k.y.zashev@gmail.com>
2021-03-16 23:28:12 +0200

    all constants in one file

---
+++ assets/js/constants.js
@@ -0,0 +1,25 @@
+// PAGE CONTAINERS
+const HOME_PAGE = getById("homeContainer");
+const ALL_NEWS_PAGE = getById("allNewsContainer");
+const POLITIC_PAGE = getById("politicsContainer");
+const WORLD_PAGE = getById("worldContainer");
+const CULTURE_PAGE = getById("cultureContainer");
+const OFFSIDE_PAGE = getById("offsideContainer");
+const HORORSCOPE_PAGE = getById("horoscope");
+const RUBRIC_PAGE = getById("rubric");
+const ADD_NEWS_PAGE = getById("addNews");
+const LOGIN_PAGE = getById("login");
+const CURRENT_NEWS_BY_USER = getById("currentNewsByUser");
+const CONTAINER_PRINT_ALL_NEWS_BY_USER = getById("containerPrintAllNewsByUser");
+const TITLE_CONTAINER_PRINT_ALL_NEWS_BY_USER = getById("AllUserNews");
+const FOOTER_CONTAINER = getById('footerContainer');
+const READ_CURRENT_NEWS_CONTAINER = getById("currentNews");
+
+// BOXES TO PRINT
+const ALL_NEWS_BOX = getById("containerToPrintAllNews");
+const POLITICS_NEWS_BOX = getById("containerToPrintPoliticsNews");
+const WORLD_NEWS_BOX = getById("containerToPrintWorldNews");
+const CULTURE_NEWS_BOX = getById("containerToPrintCultureNews");
+const OFFSIDE_NEWS_BOX = getById("containerToPrintOffsideNews");
+const HOROSCOPE_NEWS_BOX = getById("containerToPrintHoroscopeNews");
+const RUBRIC_NEWS_BOX = getById("containerToPrintRubricNews");
